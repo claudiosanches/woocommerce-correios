4d45fce26a56686ed9ee11fb1ea9d8353ee30b63
claudiosmweb <contato@claudiosmweb.com>
2014-04-21 10:29:45 -0300

    added the simulator initial scripts and updated the form

--- includes/class-wc-correios-product-shipping-simulator.php
+++ includes/class-wc-correios-product-shipping-simulator.php
@@ -15,19 +15,47 @@ class WC_Correios_Product_Shipping_Simulator {
 	public function init() {
 		if ( is_product() ) {
 			add_action( 'woocommerce_single_product_summary', array( __CLASS__, 'simulator' ), 45 );
+			add_action( 'wp_enqueue_scripts', array( $this, 'scritps' ) );
 		}
 	}
 
-	public static function simulator() {
-		$html = '<div id="wc-correios-simulator">';
-		$html .= '<p>' . __( 'Calculate shipping and delivery time estimated to your region.', 'woocommerce-correios' ) . '</p>';
-		$html .= '<form method="post" class="cart">';
-		$html .= '<input type="text" size="9" class="input-text text" placeholder="00000-000" name="zipcode" />';
-		$html .= '<button class="button alt" type="submit">' . __( 'Calculate', 'woocommerce-correios' ) .'</button>';
-		$html .= '</form>';
-		$html .= '</div>';
+	public function scritps() {
+		wp_enqueue_style( 'woocommerce-correios-simulator', plugins_url( 'assets/css/simulator.css', plugin_dir_path( __FILE__ ) ), array(), WC_Correios::VERSION, 'all' );
+		wp_enqueue_script( 'woocommerce-correios-simulator', plugins_url( 'assets/js/simulator.js', plugin_dir_path( __FILE__ ) ), array( 'jquery' ), WC_Correios::VERSION, true );
+	}
 
-		echo $html;
+	public static function simulator() {
+		global $product;
+
+		if ( $product->needs_shipping() ) {
+
+			if ( version_compare( WOOCOMMERCE_VERSION, '2.1', '>=' ) ) {
+				$height = wc_get_dimension( str_replace( '.', ',', $product->height ), 'cm' );
+				$width  = wc_get_dimension( str_replace( '.', ',', $product->width ), 'cm' );
+				$length = wc_get_dimension( str_replace( '.', ',', $product->length ), 'cm' );
+				$weight = wc_get_weight( str_replace( '.', ',', $product->weight ), 'kg' );
+			} else {
+				$height = woocommerce_get_dimension( str_replace( '.', ',', $product->height ), 'cm' );
+				$width  = woocommerce_get_dimension( str_replace( '.', ',', $product->width ), 'cm' );
+				$length = woocommerce_get_dimension( str_replace( '.', ',', $product->length ), 'cm' );
+				$weight = woocommerce_get_weight( str_replace( '.', ',', $product->weight ), 'kg' );
+			}
+
+			$html = '<div id="wc-correios-simulator">';
+			$html .= '<strong>' . __( 'Shipping and Delivery Time.', 'woocommerce-correios' ) . '</strong>';
+			$html .= '<p>' . __( 'Calculate shipping and delivery time estimated to your region.', 'woocommerce-correios' ) . '</p>';
+			$html .= '<form method="post" class="cart">';
+			$html .= '<input type="text" size="9" class="input-text text" placeholder="00000-000" id="zipcode" name="zipcode" />';
+			$html .= '<input type="hidden" name="height" value="' . $height . '" />';
+			$html .= '<input type="hidden" name="width" value="' . $width . '" />';
+			$html .= '<input type="hidden" name="length" value="' . $length . '" />';
+			$html .= '<input type="hidden" name="weight" value="' . $weight . '" />';
+			$html .= '<button class="button alt" type="submit">' . __( 'Calculate', 'woocommerce-correios' ) .'</button>';
+			$html .= '</form>';
+			$html .= '</div>';
+
+			echo $html;
+		}
 	}
 }
 
