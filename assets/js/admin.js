1a08c452ae5329e2b85ccf70b3ed7ca3c1cbbe1f
claudiosmweb <contato@claudiosmweb.com>
2014-08-12 01:02:37 -0300

    improved the admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,27 +1,27 @@
 jQuery( document ).ready( function( $ ) {
 	$( '#woocommerce_correios_display_date' ).on( 'change', function() {
-		var correiosAddDays = $( '.form-table:eq(0) tr:eq(7)' );
+		var field = $( '.form-table:eq(0) tr:eq(7)' );
 
 		if ( $( this ).is( ':checked' ) ) {
-			correiosAddDays.show();
+			field.show();
 		} else {
-			correiosAddDays.hide();
+			field.hide();
 		}
 	}).change();
 
 	$( '#woocommerce_correios_corporate_service' ).on( 'change', function() {
-		var correiosLogin    = $( '.form-table:eq(1) tr:eq(1)' ),
-			correiosPassword = $( '.form-table:eq(1) tr:eq(2)' ),
-			correiosEsedex   = $( '.form-table:eq(1) tr:eq(7)' );
+		var login    = $( '.form-table:eq(1) tr:eq(1)' ),
+			password = $( '.form-table:eq(1) tr:eq(2)' ),
+			eSedex   = $( '.form-table:eq(1) tr:eq(7)' );
 
 		if ( 'corporate' === $( this ).val() ) {
-			correiosLogin.show();
-			correiosPassword.show();
-			correiosEsedex.show();
+			login.show();
+			password.show();
+			eSedex.show();
 		} else {
-			correiosLogin.hide();
-			correiosPassword.hide();
-			correiosEsedex.hide();
+			login.hide();
+			password.hide();
+			eSedex.hide();
 		}
 	}).change();
 });
