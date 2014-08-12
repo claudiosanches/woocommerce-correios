b7513710ff0d18c5aa1c16291b61b0ade19a29f1
claudiosmweb <contato@claudiosmweb.com>
2014-08-12 01:00:54 -0300

    improved the admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,32 +1,20 @@
 jQuery( document ).ready( function( $ ) {
-	var correiosSelect = $( '#woocommerce_correios_corporate_service' ),
-		correios_val = correiosSelect.val(),
-		display_date = $( '#woocommerce_correios_display_date' ),
-		correiosAddDays = $( '.form-table:eq(0) tr:eq(7)' ),
-		correiosLogin = $( '.form-table:eq(1) tr:eq(1)' ),
-		correiosPassword = $( '.form-table:eq(1) tr:eq(2)' ),
-		correiosEsedex = $( '.form-table:eq(1) tr:eq(7)' );
+	$( '#woocommerce_correios_display_date' ).on( 'change', function() {
+		var correiosAddDays = $( '.form-table:eq(0) tr:eq(7)' );
 
-	correiosAddDays.hide();
-	correiosLogin.hide();
-	correiosPassword.hide();
-	correiosEsedex.hide();
-
-	function addtionalDaysDisplay() {
-		if ( display_date.is( ':checked' ) ) {
+		if ( $( this ).is( ':checked' ) ) {
 			correiosAddDays.show();
 		} else {
 			correiosAddDays.hide();
 		}
-	}
-	addtionalDaysDisplay();
+	}).change();
 
-	display_date.on( 'click', function() {
-		addtionalDaysDisplay();
-	} );
+	$( '#woocommerce_correios_corporate_service' ).on( 'change', function() {
+		var correiosLogin    = $( '.form-table:eq(1) tr:eq(1)' ),
+			correiosPassword = $( '.form-table:eq(1) tr:eq(2)' ),
+			correiosEsedex   = $( '.form-table:eq(1) tr:eq(7)' );
 
-	function correiosActive( correios ) {
-		if ( 'corporate' === correios ) {
+		if ( 'corporate' === $( this ).val() ) {
 			correiosLogin.show();
 			correiosPassword.show();
 			correiosEsedex.show();
@@ -35,10 +23,5 @@ jQuery( document ).ready( function( $ ) {
 			correiosPassword.hide();
 			correiosEsedex.hide();
 		}
-	}
-	correiosActive( correios_val );
-
-	correiosSelect.on( 'change', function() {
-		correiosActive( $( this ).val() );
-	} );
+	}).change();
 });
