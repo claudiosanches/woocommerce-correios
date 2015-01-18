f2bc766835a5e04f025516e8e442a55d8d516292
Claudio Sanches <contato@claudiosmweb.com>
2015-01-18 19:14:40 -0200

    Improved the tracking history title

--- templates/myaccount/tracking-history-table.php
+++ templates/myaccount/tracking-history-table.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<h2 id="wc-correios-tracking"><?php _e( 'Delivery History', 'woocommerce-correios' ); ?></h2>
+<h2 id="wc-correios-tracking"><?php _e( 'Correios Delivery History', 'woocommerce-correios' ); ?></h2>
 
 <p><?php printf( __( 'History for the tracking code: %s.', 'woocommerce-correios' ), sprintf( '<a href="http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_LINGUA=001&P_TIPO=001&P_COD_UNI=%1$s" target="_blank">%1$s</a>', $code ) ); ?></p>
 
