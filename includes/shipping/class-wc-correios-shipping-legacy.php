cbaac54c49f7f285986546055b1a3d54b2f42e31
Claudio Sanches <contato@claudiosmweb.com>
2016-06-08 23:20:45 -0300

    Fixed legacy admin and inline notices

--- includes/shipping/class-wc-correios-shipping-legacy.php
+++ includes/shipping/class-wc-correios-shipping-legacy.php
@@ -74,8 +74,6 @@ class WC_Correios_Shipping_Legacy extends WC_Shipping_Method {
 
 	/**
 	 * Admin options fields.
-	 *
-	 * @return void
 	 */
 	public function init_form_fields() {
 		$this->form_fields = array(
@@ -254,8 +252,6 @@ class WC_Correios_Shipping_Legacy extends WC_Shipping_Method {
 
 	/**
 	 * Correios options page.
-	 *
-	 * @return void
 	 */
 	public function admin_options() {
 		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
@@ -263,7 +259,24 @@ class WC_Correios_Shipping_Legacy extends WC_Shipping_Method {
 		// Call the admin scripts.
 		wp_enqueue_script( 'wc-correios', plugins_url( 'assets/js/admin' . $suffix . '.js', plugin_dir_path( __FILE__ ) ), array( 'jquery' ), '', true );
 
-		include 'views/html-admin-page.php';
+		$reviews_url = 'https://wordpress.org/support/view/plugin-reviews/woocommerce-correios?filter=5#postform';
+
+		?>
+
+		<h3><?php echo $this->method_title; ?></h3>
+
+		<?php echo $this->method_description; ?>
+
+		<?php if ( apply_filters( 'woocommerce_correios_help_message', true ) ) : ?>
+			<div class="updated woocommerce-message inline">
+				<p><?php printf( __( 'Help us keep the %s plugin free making a %s or rate %s on %s. Thank you in advance!', 'woocommerce-correios' ), '<strong>' . __( 'WooCommerce Correios', 'woocommerce-correios' ) . '</strong>', '<a href="http://claudiosmweb.com/doacoes/">' . __( 'donation', 'woocommerce-correios' ) . '</a>', '<a href="' . esc_url( $reviews_url ) . '" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a>', '<a href="' . esc_url( $reviews_url ) . '" target="_blank">' . __( 'WordPress.org', 'woocommerce-correios' ) . '</a>' ); ?></p>
+			</div>
+		<?php endif; ?>
+
+		<table class="form-table">
+			<?php $this->generate_settings_html(); ?>
+		</table>
+		<?php
 	}
 
 	/**
@@ -334,8 +347,6 @@ class WC_Correios_Shipping_Legacy extends WC_Shipping_Method {
 	 * Calculates the shipping rate.
 	 *
 	 * @param array $package Order package.
-	 *
-	 * @return void
 	 */
 	public function calculate_shipping( $package = array() ) {
 		$rates           = array();
