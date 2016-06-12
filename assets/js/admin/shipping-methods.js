6ba68f56c7d1356d8f5c9779e68b24515ccaa29a
Claudio Sanches <contato@claudiosmweb.com>
2016-06-11 21:59:01 -0300

    Fixed service_type script

--- assets/js/admin/shipping-methods.js
+++ assets/js/admin/shipping-methods.js
@@ -8,4 +8,17 @@ jQuery( document ).ready( function( $ ) {
 			field.hide();
 		}
 	}).change();
+
+	$( 'select[id$="_service_type"]' ).on( 'change', function() {
+		var login    = $( 'input[id$="_login"]' ).closest( 'tr' ),
+			password = $( 'input[id$="_password"]' ).closest( 'tr' );
+
+		if ( 'corporate' === $( this ).val() ) {
+			login.show();
+			password.show();
+		} else {
+			login.hide();
+			password.hide();
+		}
+	}).change();
 });
