02068be97617d22b3ab279d8945bb249ecd82de7
Claudio Sanches <contato@claudiosmweb.com>
2016-06-18 09:17:41 -0300

    Fixed tracing code url in emails

--- includes/emails/class-wc-correios-tracking-email.php
+++ includes/emails/class-wc-correios-tracking-email.php
@@ -109,10 +109,9 @@ class WC_Correios_Tracking_Email extends WC_Email {
 	 * @return string
 	 */
 	public function get_tracking_code_url( $tracking_code ) {
-		$options = get_option( 'woocommerce_correios_settings', array() );
-		$url     = sprintf( '<a href="http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_LINGUA=001&P_TIPO=001&P_COD_UNI=%1$s" target="_blank">%1$s</a>', $tracking_code );
+		$url = sprintf( '<a href="http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_LINGUA=001&P_TIPO=001&P_COD_UNI=%1$s" target="_blank">%1$s</a>', $tracking_code );
 
-		if ( ! empty( $options['tracking_history'] ) && 'yes' == $options['tracking_history'] ) {
+		if ( apply_filters( 'woocommerce_correios_enable_tracking_history', false ) ) {
 			$url = sprintf( '<a href="%s#wc-correios-tracking">%s</a>', $this->object->get_view_order_url(), $tracking_code );
 		}
 
