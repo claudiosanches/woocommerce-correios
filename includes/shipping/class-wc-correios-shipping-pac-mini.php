b706b864ebdf4208ca13b7da175babdff94742e3
Sergio <sergiolucianoajr@gmail.com>
2022-09-06 14:20:07 -0300

    Create class-wc-correios-shipping-pac-mini.php
    Adicionando compatibilidade com mini envios

---
+++ includes/shipping/class-wc-correios-shipping-pac-mini.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Correios PAC MINI shipping method.
+ *
+ * @package WooCommerce_Correios/Classes/Shipping
+ * @since   3.0.0
+ * @version 3.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * PAC shipping method class.
+ */
+class WC_Correios_Shipping_PAC_MINI extends WC_Correios_Shipping {
+
+	/**
+	 * Service code.
+	 * 04510 - PAC without contract.
+	 *
+	 * @var string
+	 */
+	protected $code = '04227';
+
+	/**
+	 * Corporate code.
+	 * 04669 - PAC with contract.
+	 *
+	 * @var string
+	 */
+	protected $corporate_code = '04227';
+
+	/**
+	 * Initialize PAC.
+	 *
+	 * @param int $instance_id Shipping zone instance.
+	 */
+	public function __construct( $instance_id = 0 ) {
+		$this->id           = 'correios-pac-mini';
+		$this->method_title = __( 'PAC MINI', 'woocommerce-correios' );
+		$this->more_link    = 'http://www.correios.com.br/para-voce/correios-de-a-a-z/pac-encomenda-economica';
+
+		parent::__construct( $instance_id );
+	}
+
+	/**
+	 * Get the declared value from the package.
+	 *
+	 * @param  array $package Cart package.
+	 * @return float
+	 */
+	protected function get_declared_value( $package ) {
+		if ( 18 >= $package['contents_cost'] ) {
+			return 0;
+		}
+
+		return $package['contents_cost'];
+	}
+}
