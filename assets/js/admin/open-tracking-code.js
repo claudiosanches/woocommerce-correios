fe1ce86d838ea0ec07d6d0cd588c3b99739afd13
Claudio Sanches <contato@claudiosanches.com>
2023-09-10 18:33:16 -0300

    Updated assets

--- assets/js/admin/open-tracking-code.js
+++ assets/js/admin/open-tracking-code.js
@@ -1,18 +1,19 @@
-jQuery( function( $ ) {
-
+jQuery(function ($) {
 	/**
 	 * Admin class.
 	 *
 	 * @type {Object}
 	 */
-	var WCCorreiosOpenTrackingCode = {
-
+	const WCCorreiosOpenTrackingCode = {
 		/**
 		 * Initialize actions.
 		 */
-		init: function() {
-			$( document.body )
-				.on( 'click', '.correios-tracking-code a.tracking-code-link', this.openTrackingLink );
+		init() {
+			$(document.body).on(
+				'click',
+				'.correios-tracking-code a.tracking-code-link',
+				this.openTrackingLink
+			);
 		},
 
 		/**
@@ -20,22 +21,24 @@ jQuery( function( $ ) {
 		 *
 		 * @param {Object} evt Current event.
 		 */
-		openTrackingLink: function( evt ) {
+		openTrackingLink(evt) {
 			evt.preventDefault();
 
 			// Remove old form.
-			$( '#wc-correios-tracking__form' ).remove();
+			$('#wc-correios-tracking__form').remove();
 
-			var code  = $( this ).text();
-			var form = '<form id="wc-correios-tracking__form" method="post" action="https://www2.correios.com.br/sistemas/rastreamento/resultado.cfm" target="_blank" rel="nofollow noopener noreferrer" style="display: none;">';
-			form += '<input type="hidden" name="objetos" value="' + code + '" />';
+			const code = $(this).text();
+			let form =
+				'<form id="wc-correios-tracking__form" method="post" action="https://www2.correios.com.br/sistemas/rastreamento/resultado.cfm" target="_blank" rel="nofollow noopener noreferrer" style="display: none;">';
+			form +=
+				'<input type="hidden" name="objetos" value="' + code + '" />';
 			form += '</form>';
 
-			$( 'body' ).prepend( form );
+			$('body').prepend(form);
 
 			// Submit form.
-			$( '#wc-correios-tracking__form' ).submit();
-		}
+			$('#wc-correios-tracking__form').submit();
+		},
 	};
 
 	WCCorreiosOpenTrackingCode.init();
